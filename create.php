c2a210dd4909c4b1347b97933288a142cb8c7287
ugoni <72482818+ugoni@users.noreply.github.com>
2022-01-18 12:46:48 +0900

    Add files via upload

---
+++ create.php
@@ -0,0 +1,73 @@
+<?php
+    function print_title(){
+      if(isset($_GET['id'])){
+        echo $_GET['id'];
+      }
+      else{
+        echo 'web';
+      }
+    }
+
+    function print_list() {
+      $list = scandir('./data');
+      $i = 0;
+      while($i<count($list)){
+        if($list[$i] != '.'){
+          if($list[$i] != '..'){
+            echo "<list><a href=\"index.php?id=$list[$i]\">$list[$i]</list><br>";
+          }
+        }
+        $i = $i+1;
+      }
+    }
+
+    function print_description(){
+      if(isset($_GET['id'])){
+        echo file_get_contents("data/".$_GET['id']);
+      }else{
+        echo "Hello Web";
+      }
+    }
+ ?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>
+      <?php
+        print_title();
+       ?>
+    </title>
+    <style media="screen">
+      a {
+        text-decoration: none;
+        color: gray;
+      }
+    </style>
+  </head>
+  <body>
+    <h1><a href="index.php">WEB</a></h1>
+    <ol>
+      <?php
+        print_list();
+       ?>
+    </ol>
+    <a href="create.php">create</a>
+    <form action="create_process.php" method="post">
+      <p>
+        <input type="text" name="title" placeholder="Title">
+      </p>
+      <p>
+        <textarea name="description" placeholder="Description"></textarea>
+        <input type="submit" value="Sumbit">
+      </p>
+    </form>
+    <h2><?php
+      print_title();
+     ?></h2>
+    <?php
+    print_description();
+     ?>
+  </body>
+</html>
